c3c8aec2df61a59d42c7f2f9c8c4b2884d715036
王高敏 <wanggaomin@greatmap.com.cn>
2023-04-08 22:06:59 +0800

    全局注册组件和局部注册组件

--- 01_vue_demo/src/App.vue
+++ 01_vue_demo/src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <!-- 全局注册：直接引用 -->
+  <!-- <hello-world></hello-world> -->
+
+  <!-- 局部注册：需要按照1、2、3引用 -->
+  <!-- 1 -->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+// 2
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    // 3
     HelloWorld
   }
 }
--- 01_vue_demo/src/main.js
+++ 01_vue_demo/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// 全局引用组件
+// app.component('hello-world',HelloWorld)
+
+app.mount('#app')
+
